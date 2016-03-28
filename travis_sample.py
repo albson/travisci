8be2feb39be498378940e15a19a7bdfb3109bc0c
albson <asison89@gmail.com>
2016-03-28 11:46:30 -0700

    Add tunnel identifier to desired caps

--- travis_sample.py
+++ travis_sample.py
@@ -14,7 +14,7 @@ desired_capabilities = {}
 desired_capabilities['platform'] = 'linux'
 desired_capabilities['version'] = '44'
 desired_capabilities['browserName'] = 'Chrome'
-
+desired_capabilities['tunnel-identifier'] = os.environ.get('TRAVIS_JOB_NUMBER')
 
 driver = webdriver.Remote(command_executor = ('http://' + SAUCE_USERNAME + ':' + SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub'), desired_capabilities = desired_capabilities)
 driver.implicitly_wait(30)
